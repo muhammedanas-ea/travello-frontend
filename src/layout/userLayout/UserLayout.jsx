98b4802905df65ee4cfafe8b1a6de9919fbc1b79
anasachu2000 <muhammedanasea2000@gmail.com>
2023-12-16 14:29:03 +0530

    deploy

--- src/layout/userLayout/UserLayout.jsx
+++ src/layout/userLayout/UserLayout.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/userComponents/navbar/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/userComponents/footer/footer";
+import Footer from "../../components/userComponents/footer/Footer";
 import { ToastContainer } from "react-toastify";
 
 export default function UserLayout() {
